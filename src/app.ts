82e3c457a322973645f9b5ffb14d99ab90393f3a
CitRUS <citrus4009@gmail.com>
2021-09-03 20:31:06 +0300

    feat: Add .env checking

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import { log } from "$/utils"
 
-log(`Mode: ${process.env.NODE_ENV!}`)
-log(`Message: ${process.env.MESSAGE ?? "(empty)"}`)
+if (process.env.MESSAGE) {
+    log(`Mode: ${process.env.NODE_ENV!}`)
+    log(`Message: ${process.env.MESSAGE}`)
+} else {
+    log("Create .env file with MESSAGE variable.")
+}
